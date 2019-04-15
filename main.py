d10f6413fddd3a02ef27822b6bd34cd16d3d7997
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2019-04-15 16:39:51 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import numpy as np
 
 fdr_filename = ""
 protein_filename = ""
+threshold = 0.01
+
 
 fdr = pd.read_csv("")
 peptides = pd.read_csv("")
@@ -19,8 +22,17 @@ for i in range(85, 103, 1):
         for i2 in range(i, i+6, 2):
             cluster[i2] = current
 
+proteins = {}
+
 for ind, row in peptides.iterrows():
     for i in range(7, len(peptides.columns), 1):
-
+        if peptides["Protein"] not in proteins:
+            proteins[peptides["Protein"]] = {}
+        if peptides.columns[i] not in proteins[peptides["Protein"]]:
+            proteins[peptides["Protein"]] = 0
+        if row[peptides.columns[i]] >= threshold:
+            peptides.at[ind, peptides.columns[i]] = np.nan
+        else:
+            proteins[peptides["Protein"]] += peptides.at[ind]
 
 for ind, row in peptides.iterrows():
